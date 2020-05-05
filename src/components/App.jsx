b80070e74bd0900d65937211bcc3d03310445ff5
unknown <dethgrenade@gmail.com>
2020-05-05 12:44:03 -0700

    initial user card component completed

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,34 @@
 import React, { Component } from 'react'
+import axios from 'axios'
 import GitHubCard from './GitHubCard'
 
 import '../styles/styles.less'
+import Axios from 'axios'
 
 class App extends Component {
+    constructor(){
+        super()
+
+        this.state = {
+            card: {}
+        }
+    }
+
+    componentDidMount(){
+        axios
+            .get(`https://api.github.com/users/jessemarek`)
+            .then(res => {
+                this.setState({
+                    card: res.data
+                })
+            })
+            .catch(err => console.log('ERROR: ', err))
+    }
 
     render() {
         return (
             <div>
-                <GitHubCard />
+                <GitHubCard data={this.state.card} />    
             </div>
         )
     }
--- src/components/GitHubCard.jsx
+++ src/components/GitHubCard.jsx
@@ -5,15 +5,15 @@ class GitHubCard extends Component {
     render() {
         return (
             <div className="card">
-                <img src="" />
+                <img src={this.props.data.avatar_url} />
                 <div className="card-info">
-                    <h3 className="name">Name</h3>
-                    <p className="username">username</p>
-                    <p>Location: </p>
-                    <p>Profile:<a href=""></a></p>
-                    <p>Followers: </p>
-                    <p>Following: </p>
-                    <p>Bio: </p>
+                    <h3 className="name">{this.props.data.name}</h3>
+                    <p className="username">{this.props.data.username}</p>
+                    <p>Location: {this.props.data.location}</p>
+                    <p>Profile:<a href={this.props.data.html_url}>{this.props.data.html_url}</a></p>
+                    <p>Followers: {this.props.data.followers}</p>
+                    <p>Following: {this.props.data.following}</p>
+                    <p>Bio: {this.props.data.bio}</p>
                 </div>
             </div >
         )
